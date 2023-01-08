fb817fbfb19653f70cfec735be221326d925585c
Martin <m.rodriguez0743@gmail.com>
2023-01-07 23:17:42 -0500

    Add final functionality with advanceRound() which adds 1 to every round the players start

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let humanScore = 0;
 let computerScore = 0;
-let currentRoundNumber = 1;
+let currentRoundNumber = 0;
 
 // Write your code below:
 
@@ -20,16 +20,23 @@ function compareGuesses(human, computer, target) {
     };
 };
 
-console.log(compareGuesses(1, 3, generateTarget()));
+compareGuesses(1, 3, generateTarget());
 
 function updateScore(winner) {
     if (winner === 'human') {
-       return ++humanScore + `${winner}`;
+       return ++humanScore;
     } else if (winner === 'computer') {
-        return ++computerScore + `${winner}`;
+        return ++computerScore;
     } else {
         console.log('Something went wrong.');
     };
 };
 
-console.log(updateScore('computer'));
+updateScore('human');
+
+function advanceRound() {
+    currentRoundNumber += 1;
+    return currentRoundNumber;
+};
+
+advanceRound();
